ddfe6793fece048ade5673a8fa6af3207d71002b
Brian Kelly <polymonic@gmail.com>
2011-07-28 20:30:58 -0400

    Lots of progress with dojo

--- js/set.js
+++ js/set.js
@@ -1,7 +1,10 @@
 dojo.declare("set.Game", null, {
 	deck: null,
 	table: null,
+	selected: null,
 	tableDiv: null,
+	score: 0,
+	found: new Array(),
 	
 	initializeGame: function() {
 		this.tableDiv = dojo.byId("theTable");
@@ -12,14 +15,70 @@ dojo.declare("set.Game", null, {
 		this.table = new set.Table();
 		this.table.add(this.deck.draw(12));
 
+		this.selected = new Array();
+
 		this.renderTable();
 	},
 	
 	renderTable: function() {
+		alert(this.table.cards.length);
+		dojo.empty(this.tableDiv);
 		for (i = 0; i < this.table.cards.length; i++) {
-			dojo.create("img", {src: this.table.cards[i].imageUrl}, this.tableDiv);
+			var card = dojo.create("img", {src: this.table.cards[i].imageUrl, id: i, class: "card"}, this.tableDiv);
+			dojo.connect(card, "onclick", function(evt) {
+				game.selectCard(evt);
+			});
 		}
+	},
+	
+	renderScore: function() {
+		dojo.byId("score").innerHTML = this.score;
+	},
+
+	selectCard: function(evt) {
+		var cardId = evt.target.id;
+		var card = dojo.byId(cardId);
+
+		dojo.toggleClass(card, "selected");
+		
+		this.selected = dojo.query(".selected");
+		
+		if (this.selected.length == 3) {
+			this.checkForSet();
+		}
+	},
+	
+	checkForSet: function() {
+		if (this.isValidSetSelected()) {
+			alert("Cool beans!");
+			this.found.push(this.selected);
+
+			// increase score by 1
+			this.score++;
+			
+			// remove cards from table
+			
+			// draw 3 more cards
+			this.table.add(this.deck.draw(3));
+			this.renderTable();
+			
+		} else {
+			alert("Nope!");
+			this.score--;
+		}
+		
+		this.renderScore();
+
+		this.selected.forEach(function(node, index, nodelist){
+		    dojo.removeClass(node, "selected");
+		});
+
+	},
+	
+	isValidSetSelected: function() {
+		return true;
 	}
+
 });
 
 dojo.declare("set.Table", null, {
@@ -30,7 +89,7 @@ dojo.declare("set.Table", null, {
 	},
 
 	add: function(cards) {
-		this.cards = cards;
+		this.cards = this.cards.concat(cards);
 	},
 });
 
@@ -38,13 +97,21 @@ dojo.declare("set.Deck", null, {
 	cards: new Array(),
 
 	constructor: function() {
-		for (i = 0; i < 81; i++) {
-			this.cards.push(new set.Card());
+		for (i = 0; i < 3; i++) {
+			for (j =0; j < 3; j++) {
+				for (k = 0; k < 3; k++) {
+					for (l = 0; l < 3; l++) {
+						this.cards.push(new set.Card(i,j,k,l));
+					}
+				}
+			}
 		}
 	},
 
 	shuffle: function() {
-
+		this.cards = this.cards.sort(function(a,b) {
+			return a.order < b.order;
+		});
 	},
 	
 	draw: function(count) {
@@ -57,85 +124,20 @@ dojo.declare("set.Deck", null, {
 });
 
 dojo.declare("set.Card", null, {
-	imageUrl: "images/cards/card0000.png",
+	count: null,
+	color: null,
+	symbol: null,
+	shading: null,
+	imageUrl: null,
+	order: null,
 	
-	constructor: function(args) {
-		
-	}
-});
-
-/*
-function initializeBoard() {
-	$("#message").hide();
-}
-
-function initialDraw() {
-	showMessage("Drawing cards...");
-	deck = new Deck();
-	table = new Table();
-	deck.shuffle();
-	table.add(deck.draw(12));
-	table.draw();
-}
-
-function showMessage(message) {
-	$("#message").text(message);
-	$("#message").fadeIn('slow').delay(500).fadeOut();
-}
-
-var Card = Class.create({
-	initialize: function(count, color, shading, symbol) {
+	constructor: function(count, color, symbol, shading) {
+		this.id = "" + count + color + symbol + shading;
 		this.count = count;
 		this.color = color;
-		this.shading = shading;
 		this.symbol = symbol;
-		this.imageUrl = "images/cards/card" + count + color + shading + symbol + ".png";
-	}
-})
-
-var Deck = Class.create({
-	initialize: function() {
-		this.cards = new Array();
-		this.generateDeck();
-	},
-
-	generateDeck: function() {
-		for (var count = 0; count < 3; count++) {
-			for (var color = 0; color < 3; color++) {
-				for (var shading = 0; shading < 3; shading++) {
-					for (var symbol = 0; symbol < 3; symbol++) {
-						this.cards.push(new Card(count, color, shading, symbol));
-					}
-				}
-			}
-		}		
-	},
-	
-	shuffle: function() {
-
-	},
-	
-	draw: function(count) {
-		var draw = new Array();
-		for(var i = 0; i < count; i++) {
-			draw.push(this.cards.pop());
-		}
-		return draw;
+		this.shading = shading;
+		this.imageUrl = "images/cards/card" + this.id + ".png";
+		this.order = Math.floor(Math.random()*100000);
 	}
 });
-
-var Table = new Class.create({
-	initialize: function() {
-		this.cards = new Array();
-	},
-	add: function(cards) {
-		this.cards = this.cards.concat(cards);
-	},
-	draw: function() {
-		$("#table").empty();
-		for (var i = 0; i < this.cards.length; i++) {
-			$("#table").append('<img src="' + this.cards[i].imageUrl +'"/>');
-		}
-	}
-});
-*/
